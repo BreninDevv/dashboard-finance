e3595cfb5e69a9e2f3982ff561970a7355b39b0f
Breno Henrique <brenoeevil@gmail.com>
2025-12-15 12:21:26 -0300

    study: iniciando branch de estudo sobre state e events

--- src/app/layout.jsx
+++
@@ -1,9 +0,0 @@
-import "./globals.css";
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="pt-br">
-      <body className="bg-[#e5e5ee]">{children}</body>
-    </html>
-  );
-}
