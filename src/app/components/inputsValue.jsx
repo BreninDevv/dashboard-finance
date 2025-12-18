3f272abc068d04804f65b0bd5e39b974b5b27917
Breno Henrique <brenoeevil@gmail.com>
2025-12-18 00:47:41 -0300

    feat: modal add e send feito

--- src/app/components/inputsValue.jsx
+++ src/app/components/inputsValue.jsx
@@ -41,12 +41,105 @@ const InputsValue = ({ Name, Icon }) => {
         </div>
       </div>
       {send ? (
-        <div
-          className="fixed inset-0 bg-black/40 flex items-center justify-center z-50 p-4 "
-          onClick={() => setSend(false)}
-        >
-          <div className="bg-white w-full max-w-[320px] h-auto rounded-xl shadow max-h-[90vh] overflow-auto">
-            <h1>SLV</h1>
+        <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50 p-4">
+          <div className="bg-white w-full max-w-[320px] h-auto rounded-xl shadow max-h-[90vh] overflow-auto py-4 px-6 ">
+            <div className="flex flex-col gap-4 pb-6">
+              <div className="flex justify-between">
+                <div className="flex flex-col gap-1">
+                  <p className="text-lg">Categoria do gasto</p>
+                  <select className="border rounded-md p-2">
+                    <option value="Selecione">Selecione...</option>
+                    <option value="Alimentação">Alimentação</option>
+                    <option value="Transporte">Transporte</option>
+                    <option value="Compras">Compras</option>
+                    <option value="Lazer">Lazer</option>
+                    <option value="Contas">Contas</option>
+                  </select>
+                </div>
+                <div className="text-2xl" onClick={() => setSend(false)}>
+                  X
+                </div>
+              </div>
+              <div>
+                <div>
+                  <input
+                    type="number"
+                    placeholder="R$120"
+                    className="rounded-md border p-2"
+                  />
+                </div>
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="Descrição do gasto"
+                  className="rounded-md border p-2"
+                />
+              </div>
+            </div>
+            <div className="flex justify-start  gap-2">
+              <div className="w-25 bg-green-400 text-center rounded-lg p-1 text-lg">
+                Confirm
+              </div>
+              <div
+                className="w-25 bg-gray-400 text-center rounded-lg p-1 text-lg"
+                onClick={() => setSend(false)}
+              >
+                Cancel
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <p className="hidden">Jesus é bom</p>
+      )}
+      {add ? (
+        <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50 p-4">
+          <div className="bg-white w-full max-w-[320px] h-auto rounded-xl shadow max-h-[90vh] overflow-auto py-4 px-6 ">
+            <div className="flex flex-col gap-4 pb-6">
+              <div className="flex justify-between">
+                <div className="flex flex-col gap-1">
+                  <p className="text-lg">Categoria do ganho</p>
+                  <select className="border rounded-md p-2">
+                    <option value="Selecione">Selecione...</option>
+                    <option value="Alimentação">Salário</option>
+                    <option value="Transporte">Freelance</option>
+                    <option value="Compras">Extra</option>
+                    <option value="Lazer">Outros</option>
+                  </select>
+                </div>
+                <div className="text-2xl" onClick={() => setAdd(false)}>
+                  X
+                </div>
+              </div>
+              <div>
+                <div>
+                  <input
+                    type="number"
+                    placeholder="R$120"
+                    className="rounded-md border p-2"
+                  />
+                </div>
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="Descrição do ganho"
+                  className="rounded-md border p-2"
+                />
+              </div>
+            </div>
+            <div className="flex justify-start  gap-2">
+              <div className="w-25 bg-green-400 text-center rounded-lg p-1 text-lg">
+                Confirm
+              </div>
+              <div
+                className="w-25 bg-gray-400 text-center rounded-lg p-1 text-lg"
+                onClick={() => setAdd(false)}
+              >
+                Cancel
+              </div>
+            </div>
           </div>
         </div>
       ) : (
