d548dc864d5254c1d55f5d48ce90126f74837bbc
Breno Henrique <brenoeevil@gmail.com>
2025-12-22 00:33:08 -0300

    feat: iniciando dark mode e idiomas

--- src/app/components/Header/headerDesktopComponents/enPt.jsx
+++ src/app/components/Header/headerDesktopComponents/enPt.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const EnPt = () => {
+  const [language, setLanguage] = useState(false);
+
   return (
     <>
       <div className="flex items-center gap-1.5 justify-between font-inter">
         <span className="text-2xl">En/Pt</span>
-        <div className="xl:w-16 xl:h-8 lg:w-16 lg:h-8 w-12 h-8 bg-zinc-600 rounded-2xl cursor-pointer">
+        <div
+          className={`xl:w-16 xl:h-8 lg:w-16 lg:h-8 w-12 h-8 ${
+            language ? "bg-sky-500" : "bg-zinc-600"
+          } rounded-2xl cursor-pointer`}
+          onClick={() => setLanguage((prev) => !prev)}
+        >
           <div className="h-full w-8 bg-white rounded-full scale-80"></div>
         </div>
       </div>
