c6be8c57dba4e94d9af399a3b9ea94d79470c5b8
Breno Henrique <brenoeevil@gmail.com>
2025-12-23 17:15:18 -0300

    feat: corrigindo meses nos 2 idiomas

--- src/app/components/Grafico/ChartPanel.jsx
+++ src/app/components/Grafico/ChartPanel.jsx
@@ -3,35 +3,43 @@
 import React, { useMemo } from "react";
 import Graphic from "./graphic";
 import { useTransactions } from "../../../context/TransactionsContext";
-
-const months = [
-  "jan",
-  "Fev",
-  "Mar",
-  "Abr",
-  "Mai",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
+import { useLanguage } from "../../i18n/languageContext";
 
 export default function ChartPanel({ data }) {
   const { transactions } = useTransactions();
+  const { t } = useLanguage(); // ✅ aqui é o lugar certo
+
+  const months = [
+    t.months.jan,
+    t.months.feb,
+    t.months.mar,
+    t.months.apr,
+    t.months.may,
+    t.months.jun,
+    t.months.jul,
+    t.months.aug,
+    t.months.sep,
+    t.months.oct,
+    t.months.nov,
+    t.months.dec,
+  ];
 
   const chartData = useMemo(() => {
     if (data && Array.isArray(data)) return data;
+
     const totals = new Array(12).fill(0);
-    transactions.forEach((t) => {
-      const idx = new Date(t.date).getMonth();
-      const signed = t.isExpense ? -Number(t.amount) : Number(t.amount);
+
+    transactions.forEach((tr) => {
+      const idx = new Date(tr.date).getMonth();
+      const signed = tr.isExpense ? -Number(tr.amount) : Number(tr.amount);
       totals[idx] += signed;
     });
-    return months.map((m, i) => ({ name: m, value: Math.round(totals[i]) }));
-  }, [transactions, data]);
+
+    return months.map((m, i) => ({
+      name: m,
+      value: Math.round(totals[i]),
+    }));
+  }, [transactions, data, months]);
 
   return (
     <div>
