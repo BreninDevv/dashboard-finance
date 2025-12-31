c792f850c7fa78b789e21ddfa5f89dcd895a8138
Breno Henrique <brenoeevil@gmail.com>
2025-12-31 18:24:18 -0300

    chore: tinha esquecido de uma url

--- frontend/src/app/login/login.jsx
+++ frontend/src/app/login/login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
     }
 
     try {
-      const res = await fetch("http://localhost:3333/auth/login", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
